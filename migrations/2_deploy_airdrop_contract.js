114598d40bed3fe21f450d1af8d3a85270bda219
chihyen <nlsh710599@gmail.com>
2023-01-14 13:39:10 +0800

    implement automatic deployment setting

--- migrations/2_deploy_airdrop_contract.js
+++ migrations/2_deploy_airdrop_contract.js
@@ -1,23 +1,39 @@
 const { deployProxy } = require("@openzeppelin/truffle-upgrades")
 const fs = require("fs")
+const dotenv = require("dotenv")
+dotenv.config({ path: "../.env" })
 
 const Airdrop = artifacts.require("Airdrop.sol")
 
-module.exports = async function (deployer) {
+contractParams = async (network) => {
+  let operator, startTime, endTime, eventAmount
+
+  if (network === "production") {
+    operator = process.env.operator
+    startTime = process.env.startTime
+    endTime = process.env.endTime
+    eventAmount = process.env.eventAmount
+  } else {
+    operator = (await web3.eth.getAccounts())[0]
+    startTime = 0
+    endTime = 2 * new Date().getTime()
+    eventAmount = 1
+  }
+
+  return [operator, startTime, endTime, eventAmount]
+}
+
+module.exports = async function (deployer, network) {
   try {
-    const accounts = await web3.eth.getAccounts()
-    const instance = await deployProxy(
-      Airdrop,
-      [accounts[0], 0, 2673594628764, 1],
-      {
-        deployer,
-      }
-    )
+    const instance = await deployProxy(Airdrop, await contractParams(network), {
+      deployer,
+    })
 
     const streamedInfo = JSON.stringify({
       address: instance.address,
     })
-    fs.writeFileSync(`./airdrop-contract-address.json`, streamedInfo)
+
+    fs.writeFileSync(`./airdrop-contract-address-${network}.json`, streamedInfo)
   } catch (err) {
     console.error(err)
   }
